99690dc9d9f6de654c3e61cac2e04daa0f98356d
devNishigandhaPatil <nishigandhapatil2805.com>
2022-09-12 18:07:26 +0530

    First commit

---
+++ src/com/pro/wings/Demo1.java
@@ -0,0 +1,18 @@
+package com.pro.wings;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		String s=new String ("RAM");
+		String s1=s.intern();
+		String s2="RAM";
+		
+		System.out.println(s==s1);
+		System.out.println(s1.intern()==s2);
+		System.out.println(s1.toUpperCase()==s2);
+		
+		
+	}
+
+}
